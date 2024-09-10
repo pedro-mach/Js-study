0b5369fe84d189e76c5c43d1d5dc7fccbf80a711
Pedro H. Machado <pedroh.machadodeoliveira@gmail.com>
2024-09-10 16:11:46 -0300

    Update db.js

--- Script/db.js
+++ Script/db.js
@@ -76,5 +76,80 @@ let dados = [
         especs: "V8 biturbo 4.4 L || Potência máxima: 625 cv || Torque máximo: 750 Nm || Transmissão M Steptronic de 8 velocidades || Tração integral || Design cupê esportivo",
         link: "https://www.bmw.com.br/pt/all-models/m-series/m8-gran-coupe.html",
         tag: "BMW, coupe, esportivo, motor v8, performance, alemao, tracao integral"
+    },
+    {
+        nome: "Fiat Strada",
+        specs: "1.3 Firefly Flex || 1.3 Firefly Turbo Flex || Câmbio manual ou automático |Direção elétrica || Carga útil de até 720 kg",
+        link: "https://www.fiat.com.br/carros/strada/",
+        tag: "picape, compacta, Fiat, flex, nacional, trabalho, lazer"
+    },
+    {
+        nome: "VW Polo",
+        specs: "1.0 TSI || 1.6 MSI || Câmbio manual ou automático || Motor turbo || Design moderno",
+        link: "https://www.vw.com.br/carros/polo",
+        tag: "hatch, compacto, Volkswagen, TSI, MSI, nacional, urbano"
+    },
+
+
+    {
+        nome: "Honda City",
+        specs: "1.5 i-VTEC || Câmbio CVT || Motor eficiente || Design elegante",
+        link: "https://www.honda.com.br/carros/city",
+        tag: "sedan, compacto, Honda, i-VTEC, CVT, nacional, urbano"
+    },
+    {
+        nome: "Jeep Compass",
+        specs: "1.3 Turbo Flex || 2.0 Turbo Diesel || Câmbio automático || SUV médio || Design robusto",
+        link: "https://www.jeep.com.br/compass",
+        tag: "SUV, médio, Jeep, turbo, diesel, nacional, aventura"
+    },
+    {
+        nome: "Nissan Kicks",
+        specs: "1.6 SCV+ || Câmbio CVT || SUV compacto || Design moderno || Tecnologia Nissan Connect",
+        link: "https://www.nissan.com.br/carros/kicks",
+        tag: "SUV, compacto, Nissan, SCV+, CVT, nacional, urbano"
+    },
+    {
+        nome: "Hyundai Creta",
+        specs: "1.6 MPI ou 2.0 MPI || Câmbio manual ou automático || SUV compacto || Design moderno || Tecnologia embarcada",
+        link: "https://www.hyundai.com.br/carros/creta",
+        tag: "SUV, compacto, Hyundai, MPI, nacional, urbano, familiar"
+    },
+    {
+        nome: "Fiat Toro",
+        specs: "1.3 Firefly Turbo Flex || 2.0 Multijet II Diesel || Câmbio automático || Picape média || Design robusto",
+        link: "https://www.fiat.com.br/carros/toro/",
+        tag: "picape, média, Fiat, turbo, diesel, nacional, aventura"
+    },
+    {
+        nome: "VW Saveiro",
+        specs: "1.6 MSI || Câmbio manual || Picape compacta || Robusta || Versátil",
+        link: "https://www.vw.com.br/carros/saveiro",
+        tag: "picape, compacta, Volkswagen, MSI, nacional, trabalho"
+    },
+    {
+        nome: "Chevrolet Celta (Life)",
+        specs: "1.0 MPFI || Câmbio manual || Motor eficiente || Design compacto",
+        link: "https://www.chevrolet.com.br/celta",
+        tag: "hatch, compacto, Chevrolet, MPFI, nacional, popular, econômico"
+    },
+    {
+        nome: "Chevrolet Chevette (Super)",
+        specs: "1.6 L || Câmbio manual || Motor robusto || Design clássico",
+        link: "https://www.chevrolet.com.br/chevette",
+        tag: "sedan, compacto, Chevrolet, clássico, nacional, popular"
+    },
+    {
+        nome: "Volkswagen Fusca (1968)",
+        specs: "1.2 L || Ar refrigerado || Motor traseiro || Design icônico",
+        link: "https://www.volkswagen.com.br/fusca",
+        tag: "hatch, clássico, Volkswagen, ar refrigerado, motor traseiro, icônico"
+    },
+    {
+        nome: "Opel Astra (2011)",
+        specs: "1.8 Ecotec || Câmbio manual ou automático || Design moderno || Segurança",
+        link: "https://www.opel.com.br/astra",
+        link_original: "https://www.opel.de/cars/astra.html", 
+        tag: "hatch, médio, Opel, Ecotec, nacional, segurança"
     }
 ];
